37dbc74a1327602b9ecce0d0557527bf23e90b0d
tzicatl <tzicatl@gmail.com>
2010-12-28 21:51:45 -0600

    My site theme based on diazo. Just boilerplate now

---
+++ src/tzicatl.themes.uncomplicated/README.rst
@@ -0,0 +1,12 @@
+Introduction
+============
+
+This is a theme for plone based on the collective.examples.diazo_ . But I wanted
+to do something different for this theme, so I took the Uncomplicated theme from
+FreeCSSTemplates.org_ . It has a Creative Commons Attribution 2.5 License (which
+I haven't read because I'm Lazy).
+
+So, this is it. Enjoy.
+
+  _collective.examples.diazo :: http://svn.plone.org/svn/collective/collective.examples.diazo/
+  _FreeCSSTemplates.org :: http://www.freecsstemplates.org/preview/uncomplicated/
---
+++ src/tzicatl.themes.uncomplicated/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='tzicatl.themes.uncomplicated',
+      version=version,
+      description="Diazo theme based on a free css template",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone theme diazo blue uncomplicated',
+      author='Noe Nieto',
+      author_email='tzicatl@gmail.com',
+      url='https://github.com/tzicatl/noenieto',
+      license='Custom',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tzicatl', 'tzicatl.themes'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.xdv',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
